871aa437afa885b08edac886939460a8d44124e7
Paniov Maksym <panov_ak19@nuwm.edu.ua>
2023-10-31 22:07:58 +0200

    Third_commit

--- react-page/src/Components/Header.js
+++ react-page/src/Components/Header.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import '../App.css';
+import phoneIcon from '../Images/telephone.jpg'; // Перевірте шлях до зображення
 
 function Header() {
   return (
     <div className="header">
       <div className="text-container">
-        <span className="best-text">Best</span>
-        <span className="bank-text">Bank</span>
+        <div className="best-bank">
+          <span className="best-text">Best</span>
+          <span className="bank-text">Bank</span>
+        </div>
+        <div className="phone">
+          <img src={phoneIcon} alt="Телефон іконка" />
+          <span className="phone-number">800 002 03 14</span>
+        </div>
       </div>
-      <CenteredText text="телефон" />
       <div className="right-align">
         <span className="bold">UA</span> | EN
       </div>
-    </div>
-  );
-}
-
-function CenteredText({ text }) {
-  return (
-    <div className="center">
-      {text}
+      <div className="deposit">Внесення коштів</div>
     </div>
   );
 }
